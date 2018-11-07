466d243d127f2be47d22e54a6b6056532eb0cc8e
poop-squared <44213062+poop-squared@users.noreply.github.com>
2018-11-06 23:18:11 -0500

    completed question if anyone wants to double check

--- question 2.R
+++ question 2.R
@@ -23,3 +23,28 @@ tree.q2c = rpart(winner~ county_population_2014+ population_change+
                  control = rpart.control(cp = 0.001))
 rpart.plot(tree.q2c)
 summary(tree.q2c)
+
+plotcp(tree.q2c)
+
+#2f
+
+tree.q2f  = rpart(winner~ county_population_2014+ population_change+ 
+                              percent_under_5+ percent_over_65+ percent_female+ 
+                              percent_black+ percent_hispanic+ median_household_income+ 
+                              percent_bachelor,
+                            control = rpart.control(cp = 0.00000000000000001))
+
+
+plotcp(tree.q2f)
+#identify optimal cp
+best_cp = tree.q2f$cptable[which.min(tree.q2f$cptable[,"xerror"]),"CP"]
+best_cp
+
+tree.optimal =  rpart(winner~ county_population_2014+ population_change+ 
+                        percent_under_5+ percent_over_65+ percent_female+ 
+                        percent_black+ percent_hispanic+ median_household_income+ 
+                        percent_bachelor,
+                      control = rpart.control(cp = best_cp))
+plotcp(tree.optimal)
+
+rpart.plot(tree.optimal)
