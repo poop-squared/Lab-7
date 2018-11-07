a0607fb4126ac319d3024ce27fa320d25018b158
poop-squared <44213062+poop-squared@users.noreply.github.com>
2018-11-06 22:04:17 -0500

    packages and dataframe

---
+++ general libraries.R
@@ -0,0 +1,13 @@
+### Data Frame ####
+
+election = read.csv("election_2016.csv")
+
+
+####Libraries####
+install.packages("tree")
+install.packages("rpart.plot")
+install.packages("randomForest")
+library(randomForest)
+library(tree)
+library(rpart)
+library(rpart.plot)
