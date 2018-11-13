8067e92a068b17c9c2ad8490851ee081b919077c
poop-squared <44213062+poop-squared@users.noreply.github.com>
2018-11-12 22:39:13 -0500

    fixed part C, still shitty result

--- question 1.R
+++ question 1.R
@@ -13,14 +13,20 @@ partition.tree(mytree,ordvars=c("percent_bachelor","percent_hispanic"),add=TRUE)
 legend ("topright",pch =1, legend = c("Trump","Clinton"), col = c("red","blue"))
 
 # Part C
-View(Book2)
+Book2 =data.frame(matrix(ncol = 3, nrow = 4))
+names(Book2) = c("Y","X1","X2")
+Book2[1,] = c(2.1,2,4)
+Book2[2,] = c (18.5,7,3)
+Book2[3,] = c (2,1,2)
+Book2[4,] = c (15,8,6)
+
 attach(Book2)
 
-Book2$ï..Y = as.numeric(Book2$ï..Y)
+Book2$Y = as.numeric(Book2$Y)
 Book2$X1 = as.numeric(Book2$X1)
 Book2$X2 = as.numeric(Book2$X2)
 
-mytree2 = rpart(Book2$ï..Y ~ Book2$X2 + Book2$X2, control = rpart.control(cp = 0.01))
+mytree2 = rpart(Book2$Y ~ Book2$X1 + Book2$X2, control = rpart.control(cp = 0.01))
 rpart.plot(mytree2)
 summary(mytree2)
 
