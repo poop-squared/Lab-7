36590092d4ec0bee6d9d9256bfe8967023804fb1
poop-squared <44213062+poop-squared@users.noreply.github.com>
2018-11-12 22:29:26 -0500

    copied rodrigo's code
    1C still needs work

--- question 1.R
+++ question 1.R
@@ -2,12 +2,27 @@
 
 attach(election)
 
-##1b
+# Part B
+names(election)
+attach(election)
+mytree = tree(winner ~ percent_bachelor + percent_hispanic)
 
-g1 = ggplot(aes(percent_bachelor/100,percent_hispanic, color= winner))
+plot(election$percent_bachelor, election$percent_hispanic, pch=1, xlab="% Bachelor",ylab="Hispanic", 
+     col = ifelse(election$winner == "Trump", "red", "blue"))
+partition.tree(mytree,ordvars=c("percent_bachelor","percent_hispanic"),add=TRUE)
+legend ("topright",pch =1, legend = c("Trump","Clinton"), col = c("red","blue"))
 
-g1 + geom_point()
+# Part C
+View(Book2)
+attach(Book2)
 
-plot(percent_bachelor/100, percent_hispanic)
+Book2$ï..Y = as.numeric(Book2$ï..Y)
+Book2$X1 = as.numeric(Book2$X1)
+Book2$X2 = as.numeric(Book2$X2)
 
-str(election)
+mytree2 = rpart(Book2$ï..Y ~ Book2$X2 + Book2$X2, control = rpart.control(cp = 0.01))
+rpart.plot(mytree2)
+summary(mytree2)
+
+mytree2$cptable[which.min(mytree2$cptable[, "xerror"]),"CP"]
+min(mytree2$cptable[, "xerror"])
